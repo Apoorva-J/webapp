84c84517d463a7891345cb1748ba40aa9967b0de
Apoorva Jain <jain.apo@northeastern.edu>
2023-10-17 00:22:54 -0400

    added secrets

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('admin123');FLUSH PRIVILEGES;"
-printf 'admin123\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON assignment1_db.* TO 'root'@'localhost' IDENTIFIED BY 'admin123';"
-mysql -u root -padmin123 -Bse "CREATE DATABASE assignment1_db;"
-mysql -u root -padmin123 -Bse "SHOW DATABASES;"
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PASSWORD');FLUSH PRIVILEGES;"
+printf "$PASSWORD\n n\n n\n n\n n\n n\n y\n" | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD';"
+mysql -u root -p$PASSWORD -Bse "CREATE DATABASE $DATABASE;"
+mysql -u root -p$PASSWORD -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 cd opt
