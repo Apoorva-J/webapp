b864ba55ed1f1a7917fe37ae2a3dc0a086c12652
Apoorva Jain <jain.apo@northeastern.edu>
2023-11-08 14:12:26 -0500

    modified webapp

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import route from "./routes/index.js";
 import getDataFromCsv from './services/user.js';
+import bodyParserErrorHandler from "express-body-parser-error-handler";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-route(app);
 
+route(app);
+app.use(bodyParserErrorHandler());
 getDataFromCsv();
+
 export default app;
