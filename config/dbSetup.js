8bc92065cdc89b342569a43748920562c0cb950b
jain-apo <114782619+jain-apo@users.noreply.github.com>
2023-11-28 18:20:10 -0500

    Assignment9 (#40)
    * added api
    
    * added postSubmission api
    
    * added submission api
    
    * resolved import issue
    
    * edited postSubmission API
    
    * edited postSubmission API

--- config/dbSetup.js
+++ config/dbSetup.js
@@ -12,9 +12,21 @@ import submissionModel from '../models/submissionModel.js';
 //   database: config.database.database
 // });
 
+
+// const port = 8080;
+// const host = 'localhost';
+// const user = 'root';
+// const password = 'admin123';
+// const database = 'assignment1_db';
+// const dialect = 'mysql';
+
 const sequelize = new Sequelize(
   `${config.database.dialect}://${config.database.user}:${config.database.password}@${config.database.host}/${config.database.database}`
 );
+
+// const sequelize = new Sequelize(
+//   `${dialect}://${user}:${password}@${host}/${database}`
+// );
 
 const db = {
   sequelize,
